36b08b8db73e10a9d035e10773b993858188870f
Jennifer Joseph <josephjenny490@gmail.com>
2022-07-15 16:23:53 +0100

    commit changes

---
+++ index.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/playground')
+.then(( ) =>console.log('connected to MongoDB. . .'))
+
+.catch(err =>console.error('could not connect to MongoDB. . .', err))
+
+const mongoose = require('mongoose');
