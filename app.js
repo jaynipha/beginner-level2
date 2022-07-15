f37229c1478f10860b0f8b183cc481dcce9e3f14
Jennifer Joseph <josephjenny490@gmail.com>
2022-07-15 17:43:20 +0100

    <changes>

---
+++ app.js
@@ -0,0 +1,6 @@
+// let x = 5
+// let y = 10
+function multiply(x, y){
+    return x -y
+}
+console.log(multiply(4, 8));
--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/playground')
+
 .then(( ) =>console.log('connected to MongoDB. . .'))
 
 .catch(err =>console.error('could not connect to MongoDB. . .', err))
 
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
+async function createCourse(){
+    const courseSchema = new mongoose.Schema({
+        name: string,
+        author: string,
+        tags:[string],
+        date: {type:date, default: now},
+        isPublished: Boolean
+    });
+    const result = await course.save();
+    console.log(result);
+}
+createCourse();
+
+
+
+//classes, objects
+const Course = mongoose.model('course', courseSchema)
+const course = new Course({
+    name: 'angular course',
+    author: 'mosh',
+    tags: ['node', 'frontend'],
+});
+async function getCourses(){
+    const courses = await Course
+    .find({author:'mosh', isPublished: true});
+}
