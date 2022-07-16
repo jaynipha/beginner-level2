136710f79429c472f039394a1b5281ad5f7205ff
Jennifer Joseph <josephjenny490@gmail.com>
2022-07-16 17:57:41 +0100

    mongoose methods

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
-// let x = 5
-// let y = 10
-function multiply(x, y){
-    return x -y
-}
-console.log(multiply(4, 8));
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug') ('app:db')
+const Joi = require('joi');
+const mongoose = require('mongoose');
+// const courses = require('./routes/courses');
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+// const route = require('./routes/route');
+// const authenticate = require('./authenticate')
+require('dotenv').config();
+const app = express();
+
+// app.set('view engine','pug');
+// app.set('views', './views')
+
+// console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+// console.log(`app: ${app.get ('env')}`);
+
+mongoose.connect('mongodb://localhost/playground')
+
+.then(( ) =>console.log('connected to MongoDB. . .'))
+
+.catch(err =>console.error('could not connect to MongoDB. . .', err))
+
+
+
+
+if(app.get('env')==='development'){
+    app.use(morgan('tiny'));
+    startupDebugger('morgan enabled');
+
+};
+app.use(express.json());
+app.use(helmet());
+// app.use('/api/courses/', courses);
+//  app.use('/sad', route);
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`))
+
+
--- index.js
+++ index.js
@@ -1,37 +1,50 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
 
-.then(( ) =>console.log('connected to MongoDB. . .'))
-
-.catch(err =>console.error('could not connect to MongoDB. . .', err))
-
-//const mongoose = require('mongoose');
 async function createCourse(){
-    const courseSchema = new mongoose.Schema({
-        name: string,
-        author: string,
-        tags:[string],
-        date: {type:date, default: now},
-        isPublished: Boolean
-    });
+
     const result = await course.save();
     console.log(result);
 }
 createCourse();
 
 
+const courseSchema = new mongoose.Schema({
+    name: string,
+    author: string,
+    tags:[string],
+    date: {type:date, default: now},
+    isPublished: Boolean
+});
+
+
 
 //classes, objects
-const Course = mongoose.model('course', courseSchema)
+const CoursedB = mongoose.model('course', courseSchema)
 const course = new Course({
     name: 'angular course',
     author: 'mosh',
     tags: ['node', 'frontend'],
 });
 async function getCourses(){
-    const courses = await Course
-    .find({author:'mosh', isPublished: true})
-    // Course.limit(10),
-    // .sort
-}
+    const courses = await CoursedB.find({author:'mosh', isPublished: true})
+    //.limit(10)
+    // .find()
+    // .or([{author:'mosh' }, {isPublished:true}])
+    // this carrot punctuation character is used to filter the beginning of the string we want to find
+    //for instance, if we want it to return everything written by mosh
+    //even if it is written by mosh hamedani or any surname, anywhere mosh apppears, it will return it
+    .find({author:/^mosh/})
+    //we use this to work with the end of a string($)and it is case sensitive. to make it case insensitive
+    //we will append an (i) to it
+    .find({author:/hamedani$/})
+    // if we want to find a word written anywhere (it could be in the beginning or the end or middle, 
+    //here is the syntax
+
+
+
+
+
+
+    
+};
